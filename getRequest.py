a889456145335d91085d8b1ae0bf17c9cbd44a89
Silicon50 <ndukwechukwuaja@gmail.com>
2023-09-11 11:31:25 +0100

    HNGx project

--- getRequest.py
+++ getRequest.py
@@ -3,23 +3,23 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/api', method = ['GET'])
+@app.route('/api', methods = ['GET'])
 def req():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
     current_date = datetime.utcnow().strftime('%A')
-    current__time = datetime.utcnow().strtime("%Y-%m-%dT%H:%M:%SZ")
+    current__time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     github_repo_url = 'https://github.com/Silicon50/HNGxProjects'
     github_file_url = 'https://github.com/Silicon50/HNGxProjects/blob/main/getRequest.py'
-	response_data = {
-        "slack_name" : slack_name
-        "current_date" : current_date
-        "utc_time" : current__time
-        "track": track
-        "github_file_url" : github_file_url
-        "github_repo_url" : github_repo_url
+    response_data = {
+        "slack_name" : slack_name,
+        "current_date" : current_date,
+        "utc_time" : current__time,
+        "github_file_url" : github_file_url,
+        "github_repo_url" : github_repo_url,
         "status_code" : 200
 	}
-    
+    return (response_data)
+
 if __name__ == "__main__":
     app.run()
